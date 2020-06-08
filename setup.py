2fc950ccabe38bf55ac786842c360c833e7df328
megahomyak <58137927+megahomyak@users.noreply.github.com>
2020-06-08 18:30:18 +0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+setup(
+    name = "simple_avk",
+    packages = ["simple_avk"],
+    version = "0.5",
+    license = "unlicense",
+    description = "Simple asynchronous VK API client framework by megahomyak",
+    author = "megahomyak",
+    author_email = "g.megahomyak@gmail.com",
+    url = "https://github.com/megahomyak/simple_avk",
+    download_url = "https://github.com/megahomyak/simple_avk/archive/v_05.tar.gz",
+    keywords = ["SIMPLE", "ASYNCHRONOUS", "VK", "API"],
+    install_requires = [],
+    classifiers = [
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Application Frameworks",
+        "License :: Freeware",
+        "Programming Language :: Python :: 3"
+    ]
+)
